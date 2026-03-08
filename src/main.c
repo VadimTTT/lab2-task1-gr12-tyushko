c11f28e2646353e75e6d4d9cee47373ae29ec000
VadimTTT <137997863+VadimTTT@users.noreply.github.com>
2026-03-08 09:36:53 +0000

    feat: add main menu with command line args

--- src/main.c
+++ src/main.c
@@ -1,21 +1,28 @@
-/* hello.c */
-#include <stdio.h>
-
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
-}
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-// Декларации функций из других файлов
+// Декларации функций
 void task1_primes();
-void task2_train();
-void task3_software();
+void task2_train(int argc, char* argv[]);
+void task3_software(int argc, char* argv[]);
 
-int main() {
+int main(int argc, char* argv[]) {
+		// Инициализация генератора случайных чисел
+		srand(time(NULL));
+
 		int choice;
+
+		printf("========================================\n");
+		printf("Лабораторная работа №2\n");
+		printf("Вариант 22\n");
+		printf("Студент: Тюшко Вадим\n");
+		printf("========================================\n");
+
+		printf("\nАргументы командной строки: count = %d\n", argc);
+		for (int i = 0; i < argc; i++) {
+				printf("  argv[%d] = %s\n", i, argv[i]);
+		}
 
 		do {
 				printf("\n========== МЕНЮ ==========\n");
@@ -23,6 +30,7 @@ int main() {
 				printf("2. Структура TRAIN (поезда)\n");
 				printf("3. Структура SOFTWARE (ПО)\n");
 				printf("0. Выход\n");
+				printf("==========================\n");
 				printf("Выберите задание: ");
 				scanf("%d", &choice);
 
@@ -31,16 +39,16 @@ int main() {
 								task1_primes();
 								break;
 						case 2:
-								task2_train();
+								task2_train(argc, argv);
 								break;
 						case 3:
-								task3_software();
+								task3_software(argc, argv);
 								break;
 						case 0:
-								printf("Выход...\n");
+								printf("Выход из программы...\n");
 								break;
 						default:
-								printf("Неверный выбор!\n");
+								printf("Ошибка: неверный выбор! Введите 0-3\n");
 				}
 		} while(choice != 0);
 
